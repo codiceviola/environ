9f2ddf513e91e68c605c59e9138b8ae66480302b
Puria Nafisi Azizi <puria@dyne.org>
2020-11-01 16:55:59 +0100

    feat: ✨  Take care of italian preposition in titling

--- environ/wrangler.py
+++ environ/wrangler.py
@@ -9,6 +9,83 @@ One single method for to manage the files.
 
 import pandas
 
+PREPOSITIONS = [
+    "il",
+    "lo",
+    "la",
+    "i",
+    "gli",
+    "le",
+    "di",
+    "del",
+    "dello",
+    "della",
+    "dei",
+    "degli",
+    "delle",
+    "a",
+    "al",
+    "allo",
+    "alla",
+    "ai",
+    "agli",
+    "alle",
+    "da",
+    "dal",
+    "dallo",
+    "dalla",
+    "dai",
+    "dagli",
+    "dalle",
+    "in",
+    "nel",
+    "nello",
+    "nella",
+    "nei",
+    "negli",
+    "nelle",
+    "con",
+    "col",
+    "cóllo",
+    "cólla",
+    "coi",
+    "cogli",
+    "cólle",
+    "su",
+    "sul",
+    "sullo",
+    "sulla",
+    "sui",
+    "sugli",
+    "sulle",
+    "per",
+    "pel",
+    "pello",
+    "pella",
+    "pei",
+    "pegli",
+    "pelle",
+]
+
+
+def italian_title(name):
+    """
+    Italian titling.
+
+    :param name: The name to be titled
+    :return:
+    """
+
+    def __prep_filter(e):
+        if "'" in e:
+            particles = e.casefold().split("'", 1)
+            if next((True for _ in PREPOSITIONS if particles[0] in _[:-1]), None):
+                return "'".join([particles[0].lower(), particles[1].title()])
+            return "'".join([particles[0].title(), particles[1].title()])
+        return e.lower() if e.casefold() in PREPOSITIONS else e.capitalize()
+
+    return " ".join(map(__prep_filter, name.split()))
+
 
 def clean_city_name(entry):
     """
@@ -16,10 +93,8 @@ def clean_city_name(entry):
 
     @:param entry the city name to clean
     """
-    if " (" in entry:
-        return entry[: entry.find(" (")].capitalize()
-    else:
-        return entry.capitalize()
+    entry = entry[: entry.find(" (")] if " (" in entry else entry
+    return italian_title(entry)
 
 
 def wrangle(src, dest):
@@ -30,14 +105,12 @@ def wrangle(src, dest):
     @:param dst the file path destination output
     """
     dataframe = pandas.read_csv(src)
-    dataframe = (
-        dataframe.applymap(clean_city_name())
-        .drop_duplicates(subset=["Città"])
-        .drop("X")
-        .set_index("Città")
-        .sort_index()
-    )
+    dataframe = dataframe.applymap(clean_city_name)
+    dataframe = dataframe.drop_duplicates(subset=["Città"])
+    dataframe = dataframe.set_index("Città")
+    dataframe = dataframe.sort_index()
     dataframe.to_csv(dest, index=True)
+    print(f"{src} => {dest}")
 
 
 wrangle("data/residenza.csv", "output/residenza.csv")
