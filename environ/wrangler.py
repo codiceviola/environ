ed98b00d87529ec52af53fb1965a5a4f5be6fd3c
Puria Nafisi Azizi <puria@dyne.org>
2021-02-12 15:42:20 +0100

    Adding ispra table per municipality

--- environ/wrangler.py
+++ environ/wrangler.py
@@ -115,6 +115,43 @@ def wrangle(src, dest):
     print(f"{src} => {dest}")
 
 
+def ispra_per_municipality(src, dest):
+    """Parse the ISPRA datas from https://annuario.isprambiente.it/sites/default/files/sys_ind_files/indicatori_ada/124/TABELLA%201_PM10_2018_new.xlsx."""
+    dataframe = pandas.read_excel(src)
+    dataframe = dataframe.drop(
+        [
+            "Nome della stazione",
+            "Unnamed: 0",
+            "Unnamed: 1",
+            "Tipo di zona",
+            "Tipo di stazione",
+            "Tecnica di misura",
+            "Valore medio1,3 annuo",
+            "50° percentile1 ",
+            "75° percentile2 ",
+            "90,4° percentile2",
+            "98°  percentile2 ",
+            "99,2° percentile2",
+            "Valore massimo2 ",
+            "Numero di dati validi",
+            "AQD used4",
+        ],
+        axis=1,
+    )
+    dataframe = dataframe.drop(dataframe.index[[0]])
+    dataframe = dataframe.rename(columns={"Giorni di superamento di 50 µg/m3": "Limit days"})
+    dirty_limits = dataframe[dataframe["Limit days"] == "-"]
+    dataframe = dataframe.drop(dirty_limits.index)
+    dataframe["Limit days"] = dataframe["Limit days"].astype(int)
+    dataframe = dataframe[dataframe["Limit days"] >= 35]
+    dataframe = dataframe.drop(["Limit days"], axis=1)
+    dataframe = dataframe.drop_duplicates(subset=["Comune"])
+    dataframe = dataframe.set_index("Comune")
+    dataframe = dataframe.sort_index()
+    dataframe.to_csv(dest, index=True)
+    print(f"{src} => {dest}")
+
+
 def ispra(src, dest):
     """Parse the ISPRA datas from https://annuario.isprambiente.it/sites/default/files/sys_ind_files/indicatori_ada/124/TABELLA%201_PM10_2018_new.xlsx."""
     dataframe = pandas.read_excel(src)
@@ -155,3 +192,4 @@ def ispra(src, dest):
 wrangle("data/residenza.csv", "output/residenza.csv")
 wrangle("data/nascita.csv", "output/nascita.csv")
 ispra("data/TABELLA 1_PM10_2018_new.xlsx", "output/ispra.csv")
+ispra_per_municipality("data/TABELLA 1_PM10_2018_new.xlsx", "output/ispra_comune.csv")
